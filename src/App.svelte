143652ddcf5f77aabedba979424367f9571952d5
shakilkhan12 <shakilkhan621@gmail.com>
2020-04-30 08:34:24 -0700

    upload to github

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Header from "./components/Header.svelte"
+	import Footer from "./components/Footer.svelte"
+	import Tabs from "./shared/Tabs.svelte"
+
+	// Tabs
+	let items = ['Current Polls', "Add New Poll"]
+	let activeItem = "Current Polls"
+	const tabUpdate = (e) => {
+		activeItem = e.detail;
+		
+	}
+</script>
+
+<Header />
+<main>
+	<Tabs {items} {activeItem} on:tabChange={tabUpdate} />
+	{#if activeItem === 'Current Polls'}
+	  <p>Poll list component here</p>
+	  {:else if activeItem === 'Add New Poll' }
+	  <p>New poll form goes here</p>
+	{/if}
+</main>
+<Footer />
+<style>
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,18 @@
+<header>
+<h1>
+<img src="/img/poll_ninja_logo.svg" alt="Poll logo">
+</h1>
+</header>
+<style>
+  header {
+      background: #F7F7F7;
+      padding: 20px;
+  }
+  h1 {
+      margin: 0;
+      text-align: center;
+  }
+  img {
+      max-width: 180px;
+  }
+</style>
